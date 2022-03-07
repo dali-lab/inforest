5e689c38609d80f21d318bf89777390422c95663
Ziray Hao <ziray.hao@gmail.com>
2022-03-07 13:47:30 -0500

    Remove boilerplate code

--- packages/app/App.tsx
+++ packages/app/App.tsx
@@ -3,15 +3,12 @@ import { SafeAreaProvider } from "react-native-safe-area-context";
 import { Provider } from "react-redux";
 
 import useCachedResources from "./hooks/useCachedResources";
-import useColorScheme from "./hooks/useColorScheme";
-import Navigation from "./navigation";
 import store from "./redux";
 import { persistStore } from "redux-persist";
 import { PersistGate } from "redux-persist/integration/react";
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
-  const colorScheme = useColorScheme();
 
   const persistedStore = persistStore(store);
 
