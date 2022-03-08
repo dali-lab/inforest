9502173b14d24f7718616825625e2b6c9c09db78
Ziray Hao <ziray.hao@gmail.com>
2022-03-08 00:03:37 -0500

    Fix sequelize-cli config connection string

--- packages/server/src/db/config/config.js
+++ packages/server/src/db/config/config.js
@@ -4,17 +4,23 @@ dotenv.config();
 
 module.exports = {
   development: {
-    url: `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
+    url:
+      process.env.DATABASE_URL ??
+      `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
     dialect: "postgres",
     logging: true,
   },
   test: {
-    url: `postgres://${process.env.TEST_DB_NAME}:${process.env.TEST_DB_PASSWORD}@${process.env.TEST_DB_HOST}:${process.env.TEST_DB_PORT}/${process.env.TEST_DB_NAME}`,
+    url:
+      process.env.DATABASE_URL ??
+      `postgres://${process.env.TEST_DB_NAME}:${process.env.TEST_DB_PASSWORD}@${process.env.TEST_DB_HOST}:${process.env.TEST_DB_PORT}/${process.env.TEST_DB_NAME}`,
     dialect: "postgres",
     logging: false,
   },
   production: {
-    url: `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
+    url:
+      process.env.DATABASE_URL ??
+      `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
     dialect: "postgres",
     logging: false,
   },
