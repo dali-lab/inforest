93ab60e8861548e0858486a091ed2f3c4415af05
Ziray Hao <ziray.hao@gmail.com>
2022-03-08 16:36:32 -0500

    Build data entry flow

--- packages/app/components/PlotDrawer.tsx
+++ packages/app/components/PlotDrawer.tsx
@@ -1,11 +1,11 @@
-import React, { useCallback, useEffect } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { Forest, Plot, Tree } from "@ong-forestry/schema";
 import {
+  Animated,
   Button,
   Dimensions,
   Pressable,
   StyleSheet,
-  Text,
   View,
 } from "react-native";
 import {
@@ -16,8 +16,185 @@ import {
 } from "../constants";
 import Colors from "../constants/Colors";
 import useAppSelector from "../hooks/useAppSelector";
-import { deleteDraftedTree } from "../redux/slices/treeSlice";
+import {
+  deleteDraftedTree,
+  deselectTree,
+  updateTree,
+} from "../redux/slices/treeSlice";
 import DrawerButton from "./DrawerButton";
+import { Text, TextVariants } from "./Themed";
+import { BlurView } from "expo-blur";
+import { DataField } from "./DataField";
+import useAppDispatch from "../hooks/useAppDispatch";
+import { Queue, Stack } from "react-native-spacing-system";
+import dateformat from "dateformat";
+
+interface DataEntryFormProps {
+  cancel: () => void;
+  finish: () => void;
+}
+
+const DataEntryForm: React.FC<DataEntryFormProps & View["props"]> = ({
+  cancel,
+  finish,
+  style,
+}) => {
+  const dispatch = useAppDispatch();
+  const { all, selected: selectedTreeTag } = useAppSelector(
+    (state) => state.trees
+  );
+  const selected = !!selectedTreeTag ? all[selectedTreeTag] : undefined;
+  const [step, setStep] = useState(0);
+  if (!selected) {
+    return null;
+  }
+  switch (step) {
+    case 0: {
+      return (
+        <View style={[style, sstyles.container]}>
+          <View style={{ flexDirection: "row" }}>
+            <DataField
+              type="INTEGER"
+              label="Plot #"
+              value={selected.plotNumber}
+              moreInfo="Plot that this tree belongs to."
+              style={{ flex: 1 }}
+              editable={false}
+            ></DataField>
+            <Queue size={12}></Queue>
+            <DataField
+              type="SHORT_TEXT"
+              label="Tag"
+              value={selected.tag}
+              moreInfo="Unique tag identifier for this tree."
+              style={{ flex: 1 }}
+              onUpdate={(newValue) => {
+                dispatch(
+                  updateTree({
+                    tag: selected.tag,
+                    updates: { ...selected, tag: newValue },
+                  })
+                );
+              }}
+              editable={false}
+            ></DataField>
+            <Queue size={12}></Queue>
+            <DataField
+              type="DECIMAL"
+              label="Position"
+              value={`${selected.plotX}m, ${selected.plotY}m`}
+              moreInfo="Position in meters within the plot."
+              style={{ flex: 1 }}
+              editable={false}
+            ></DataField>
+          </View>
+          <Stack size={24}></Stack>
+          <View style={{ flexDirection: "row" }}>
+            <DataField
+              type={"DECIMAL"}
+              label="DBH"
+              value={selected.dbh}
+              placeholder={0.0}
+              moreInfo="Tree trunk diameter in centimeters at breast height."
+              style={{ flex: 1 }}
+              onUpdate={(newValue) => {
+                dispatch(
+                  updateTree({
+                    tag: selected.tag,
+                    updates: { ...selected, dbh: newValue },
+                  })
+                );
+              }}
+            ></DataField>
+            <Queue size={12}></Queue>
+            <DataField
+              type={"SHORT_TEXT"}
+              label="Species code"
+              value={selected.speciesCode}
+              placeholder="None"
+              moreInfo="Code for the tree species."
+              style={{ flex: 1 }}
+              onUpdate={(newValue) => {
+                dispatch(
+                  updateTree({
+                    tag: selected.tag,
+                    updates: { ...selected, speciesCode: newValue },
+                  })
+                );
+              }}
+            ></DataField>
+            <Queue size={12}></Queue>
+            <DataField
+              type={"SHORT_TEXT"}
+              label="Labels"
+              value={undefined}
+              placeholder="None"
+              moreInfo="Characteristic labels for this tree."
+              style={{ flex: 3 }}
+              onUpdate={(newValue) => {
+                dispatch(
+                  updateTree({
+                    tag: selected.tag,
+                    updates: { ...selected },
+                  })
+                );
+              }}
+            ></DataField>
+          </View>
+          <Stack size={24}></Stack>
+          <View style={{ flexDirection: "row" }}>
+            <DataField
+              type={"LONG_TEXT"}
+              label="Notes"
+              value={undefined}
+              placeholder="Jot down field notes here."
+              moreInfo="Field notes about this tree."
+              style={{ flex: 5 }}
+              onUpdate={(newValue) => {
+                dispatch(
+                  updateTree({
+                    tag: selected.tag,
+                    updates: { ...selected },
+                  })
+                );
+              }}
+            ></DataField>
+          </View>
+          <Stack size={24}></Stack>
+          <View style={sstyles.footer}>
+            {/* <DrawerButton
+              onPress={() => setStep(Math.max(0, step - 1))}
+              disabled
+            >
+              Back
+            </DrawerButton> */}
+            <Button
+              onPress={cancel}
+              title="Delete"
+              color={Colors.error}
+            ></Button>
+            <DrawerButton onPress={finish}>Save</DrawerButton>
+          </View>
+        </View>
+      );
+    }
+    case 1: {
+      return null;
+    }
+    default:
+      return null;
+  }
+};
+
+const sstyles = StyleSheet.create({
+  container: {
+    justifyContent: "space-between",
+  },
+  footer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+});
 
 interface PlotDrawerProps {
   mode: MapScreenModes;
@@ -28,6 +205,7 @@ interface PlotDrawerProps {
   openVisualizationModal: () => void;
   beginPlotting: () => void;
   endPlotting: () => void;
+  expandDrawer: () => void;
   minimizeDrawer: () => void;
 }
 
@@ -39,6 +217,7 @@ export const PlotDrawer: React.FC<PlotDrawerProps> = ({
   openVisualizationModal,
   beginPlotting,
   endPlotting,
+  expandDrawer,
   minimizeDrawer,
 }) => {
   useEffect(() => {
@@ -66,77 +245,90 @@ export const PlotDrawer: React.FC<PlotDrawerProps> = ({
         return styles.containerExpanded;
     }
   }, [drawerState]);
+
+  if (drawerState === DrawerStates.Closed) {
+    return null;
+  }
+
   return (
-    <View
+    <Animated.View
       style={{ ...styles.container, ...setStyle() }}
-      onLayout={(e) =>
-        setDrawerHeight(
-          drawerState === "CLOSED" ? 0 : e.nativeEvent.layout.height
-        )
-      }
+      onLayout={(e) => {
+        setDrawerHeight(e.nativeEvent.layout.height);
+      }}
     >
-      {drawerState !== "CLOSED" && mode === MapScreenModes.Select && (
-        <View style={styles.header}>
-          <Text>Plot #{plot?.number}</Text>
-          {drawerState === "MINIMIZED" && (
-            <DrawerButton onPress={beginPlotting}>Add Trees</DrawerButton>
-          )}
-          {/* {mode === 'EXPANDED' && (
-						<Button onPress={() => {
-							setMode('MINIMIZED')
-							onClose()
-						}} title='Back to plots'></Button>
-					)} */}
-        </View>
-      )}
-      {drawerState !== "CLOSED" && mode === MapScreenModes.Explore && (
-        <View style={[styles.header, { justifyContent: "center" }]}>
-          <DrawerButton onPress={openVisualizationModal}>
-            Visualization Settings
-          </DrawerButton>
-        </View>
-      )}
-      {drawerState !== "CLOSED" && mode === MapScreenModes.Plot && (
-        <>
+      <BlurView style={styles.blurContainer} intensity={40}>
+        {mode === MapScreenModes.Select && (
           <View style={styles.header}>
-            {drawerState === "MINIMIZED" && !!selected && (
-              <Text>
-                Tap anywhere to create a new tree in Plot #{plot?.number}
-              </Text>
+            <Text variant={TextVariants.H2}>Plot #{plot?.number}</Text>
+            {drawerState === "MINIMIZED" && (
+              <DrawerButton onPress={beginPlotting}>Add Trees</DrawerButton>
             )}
-            {drawerState === "MINIMIZED" && !!selected && (
-              <Text>
-                Tree #{selected.tag} in Plot #{plot?.number}
-              </Text>
-            )}
+          </View>
+        )}
+        {mode === MapScreenModes.Explore && (
+          <View style={[styles.header, { justifyContent: "center" }]}>
+            <DrawerButton onPress={openVisualizationModal}>
+              Visualization Settings
+            </DrawerButton>
+          </View>
+        )}
+        {mode === MapScreenModes.Plot && !!plot && (
+          <>
+            <View style={styles.header}>
+              {drawerState === "MINIMIZED" && !selected && (
+                <View style={{ flexDirection: "row", alignItems: "center" }}>
+                  <Text variant={TextVariants.H1}>Plot #{plot.number}</Text>
+                  <Queue size={36}></Queue>
+                  <Text variant={TextVariants.Body}>
+                    Tap anywhere to plot a new tree
+                  </Text>
+                </View>
+              )}
+              {drawerState === "MINIMIZED" && !!selected && (
+                <>
+                  <View style={{ flexDirection: "row", alignItems: "center" }}>
+                    <Text variant={TextVariants.H1}>Tree #{selected.tag}</Text>
+                    <Queue size={36}></Queue>
+                    <Text variant={TextVariants.Body}>
+                      Last census on{" "}
+                      {dateformat(new Date(), 'mmm dS, yyyy "at" h:MM TT')}
+                    </Text>
+                  </View>
+                  <DrawerButton onPress={expandDrawer}>Edit</DrawerButton>
+                </>
+              )}
+              {drawerState === "EXPANDED" && !!selected && (
+                <>
+                  <Text variant={TextVariants.H1}>New tree</Text>
+                  <Text variant={TextVariants.Body}>
+                    This is the {byPlots[plot.number].size + 1}th tree in the
+                    plot.
+                  </Text>
+                </>
+              )}
+            </View>
+
             {drawerState === "EXPANDED" && !!selected && (
               <>
-                <Text>New tree</Text>
-                <View style={{ flexDirection: "row" }}>
-                  <Button
-                    onPress={() => {
-                      deleteDraftedTree(selected.tag);
+                <Stack size={24}></Stack>
+                <View style={styles.content}>
+                  <DataEntryForm
+                    cancel={() => {
+                      dispatch(deleteDraftedTree(selected.tag));
+                      dispatch(deselectTree());
                       minimizeDrawer();
                     }}
-                    title="Delete tree"
-                    color={Colors.error}
-                  ></Button>
-                  <Button
-                    onPress={() => {
-                      minimizeDrawer();
-                    }}
-                    title="Save tree"
-                  ></Button>
+                    finish={minimizeDrawer}
+                    style={{ flex: 1 }}
+                  />
                 </View>
               </>
             )}
-          </View>
-          <>
-            {drawerState === "EXPANDED" && <View style={styles.content}></View>}
           </>
-        </>
-      )}
-    </View>
+        )}
+      </BlurView>
+    </Animated.View>
   );
 };
 
@@ -146,10 +338,15 @@ const styles = StyleSheet.create({
     bottom: 0,
     zIndex: 2,
     width: Dimensions.get("window").width,
-    padding: 24,
-    backgroundColor: "rgba(255, 255, 255, 0.62)",
+    backgroundColor: "rgba(255, 255, 255, 0.4)",
     borderTopLeftRadius: 24,
     borderTopRightRadius: 24,
+    overflow: "hidden",
+  },
+  blurContainer: {
+    width: "100%",
+    height: "100%",
+    padding: 24,
   },
   containerClosed: {
     display: "none",
@@ -160,9 +357,8 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
-    height: 48,
   },
   content: {
-    height: 512,
+    // height: 512,
   },
 });
