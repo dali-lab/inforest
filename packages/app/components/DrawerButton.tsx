92ddd57515c0254ac2c54bae25ea1627fe2acd9a
Ziray Hao <ziray.hao@gmail.com>
2022-03-08 16:33:09 -0500

    Add disabled state to DrawerButton

--- packages/app/components/DrawerButton.tsx
+++ packages/app/components/DrawerButton.tsx
@@ -4,32 +4,43 @@ import {
   Pressable,
   StyleProp,
   StyleSheet,
-  Text,
   View,
   ViewStyle,
 } from "react-native";
+import Colors from "../constants/Colors";
+import { Text, TextVariants } from "./Themed";
 
 interface DrawerButtonProps {
   children: React.ReactNode;
   onPress: (event: GestureResponderEvent) => void;
+  disabled?: boolean;
   style?: StyleProp<ViewStyle>;
 }
 
 const DrawerButton: React.FC<DrawerButtonProps> = ({
   children,
   onPress,
+  disabled = false,
   style,
 }) => {
   return (
-    <Pressable style={[style, styles.button]} onPress={onPress}>
-      <Text style={styles.text}>{children}</Text>
+    <Pressable
+      style={[style, styles.button]}
+      onPress={!disabled ? onPress : undefined}
+    >
+      <Text
+        variant={TextVariants.Label}
+        color={disabled ? Colors.neutral[4] : undefined}
+      >
+        {children}
+      </Text>
     </Pressable>
   );
 };
 
 const styles = StyleSheet.create({
   button: {
-    backgroundColor: "#FFFFFF",
+    backgroundColor: "white",
     borderRadius: 11,
     height: 48,
     alignItems: "center",
