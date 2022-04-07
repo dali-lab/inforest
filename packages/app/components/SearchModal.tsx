f1b5a7ee3d2825eb2745fa8c8a155607d34b2b84
Julian George <juliancgeorge@gmail.com>
2022-04-07 18:58:01 -0400

    reorganizing files, cutting down size and linting

---
+++ packages/app/components/SearchModal.tsx
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import { Modal, Pressable, StyleSheet, TextInput, View } from "react-native";
+
+interface SearchModalProps {
+  open: boolean;
+  onExit: () => void;
+  onSubmit: (searchValue: string) => void;
+}
+
+const SearchModal: React.FC<SearchModalProps> = (props) => {
+  const { open, onExit, onSubmit } = props;
+  const [searchValue, setSearchValue] = useState<string>("");
+  return (
+    <Modal visible={open} transparent={true} animationType="fade">
+      <Pressable style={styles.centeredView} onPress={onExit}>
+        <View style={[styles.modal, styles.modalContainer]}>
+          <TextInput
+            value={searchValue}
+            onChangeText={(text) => setSearchValue(text)}
+            onSubmitEditing={() => {
+              onSubmit("searchValue");
+              setSearchValue("");
+            }}
+            placeholder="Search for a tree by tag #"
+            returnKeyType="search"
+            style={{ width: 256 }}
+            autoFocus={true}
+          ></TextInput>
+        </View>
+      </Pressable>
+    </Modal>
+  );
+};
+
+const styles = StyleSheet.create({
+  modalContainer: {
+    backgroundColor: "white",
+    padding: 12,
+    borderRadius: 12,
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0,0,0,0.5)",
+  },
+  modal: {
+    alignSelf: "center",
+    shadowColor: "black",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+});
+
+export default SearchModal;
