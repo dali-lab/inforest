f30866e08df64962dba01e753838f71af1462158
Ziray Hao <ziray.hao@gmail.com>
2022-04-18 19:15:57 -0400

    Add query params to earger load association

--- packages/server/src/services/tree-service.ts
+++ packages/server/src/services/tree-service.ts
@@ -1,4 +1,5 @@
 import { Tree } from "@ong-forestry/schema";
+import { TreeCensus } from "db/models";
 import TreeModel from "db/models/tree";
 import { Op } from "sequelize";
 
@@ -170,7 +171,11 @@ export const editTrees = async (
 
 export const getTrees = async (params: GetTreesParams) => {
   const query = constructQuery(params);
-  return await TreeModel.findAll(query);
+  return await TreeModel.findAll({
+    ...query,
+    include: TreeCensus,
+    order: [[TreeCensus, "createdAt", "DESC"]],
+  });
 };
 
 export const deleteTrees = async (params: GetTreesParams) => {
