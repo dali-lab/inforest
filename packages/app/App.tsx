f05bbd2f8cb31c50a36fd25bbc62baf60932e189
Ziray Hao <ziray.hao@gmail.com>
2022-05-10 11:27:30 -0400

    Lock screen orientation
    This still won't work for Expo app, only once we build for native.

--- packages/app/App.tsx
+++ packages/app/App.tsx
@@ -1,15 +1,22 @@
 import { StatusBar } from "expo-status-bar";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { Provider } from "react-redux";
+import * as ScreenOrientation from "expo-screen-orientation";
 
 import useCachedResources from "./hooks/useCachedResources";
 import store from "./redux";
 import MapScreen from "./screens/MapScreen";
+import { useEffect } from "react";
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
 
   // const persistedStore = persistStore(store);
+
+  useEffect(() => {
+    ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.PORTRAIT_UP);
+  }, []);
+
   return (
     <Provider store={store}>
       {/* <PersistGate loading={null} persistor={persistedStore}> */}
