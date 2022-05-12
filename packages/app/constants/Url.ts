2a01f3eaefec4be8cb7bfead5e7a07ff4a06ae85
Ziray Hao <ziray.hao@gmail.com>
2022-05-12 12:41:30 -0400

    temp

--- packages/app/constants/Url.ts
+++ packages/app/constants/Url.ts
@@ -1,7 +1,5 @@
 import { LOCAL_SERVER_URL } from "./dev";
 
-const SERVER_URL = __DEV__
-  ? LOCAL_SERVER_URL
-  : "https://inforest.herokuapp.com/";
+const SERVER_URL = __DEV__ ? LOCAL_SERVER_URL : "http://100.97.244.92:3000/";
 
 export default SERVER_URL;
