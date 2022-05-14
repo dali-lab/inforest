d2359a0e2927f60cb4d7ee4cfe825fd41c295634
miruna <miruna.palaghean@gmail.com>
2022-05-14 12:49:03 -0400

    email service

--- packages/server/src/services/email-service.ts
+++ packages/server/src/services/email-service.ts
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
-import { SendEmailCommand, SESv2Client } from "@aws-sdk/client-sesv2";
+import fs from "fs";
+import { SendEmailCommand, SESv2, SESv2Client } from "@aws-sdk/client-sesv2";
 import { getUsers } from "services";
 import { getTeams } from "./team-service";
 
@@ -17,20 +18,35 @@ export const emailCode = async (verificationCode: {
     throw new Error("This user has already been verified.");
   }
 
+  // create SESv2 object to create template
+  const sesv2 = new SESv2({ region: process.env.REGION });
+
+  // try to create template; might already exist?
+  try {
+    const emailBody = fs.readFileSync("../res/code-email.html", "utf8");
+
+    const template = {
+      Subject: "Verify your Inforest account email",
+      Html: emailBody,
+    };
+
+    sesv2.createEmailTemplate({
+      TemplateName: "inforest-verification-code",
+      TemplateContent: template,
+    });
+  } catch (e: any) {
+    console.log("template exists?");
+    console.log(e);
+  }
+
+  // send email
   const client = new SESv2Client({ region: process.env.REGION });
 
   const command = new SendEmailCommand({
     Content: {
-      Template: { TemplateData: "" },
-      Simple: {
-        Body: {
-          Html: {
-            Data: "",
-          },
-        },
-        Subject: {
-          Data: "Verify your Inforest account email",
-        },
+      Template: {
+        TemplateName: "inforest-verification-code",
+        TemplateData: '{ "code":"' + verificationCode.code + '" }',
       },
     },
     Destination: { ToAddresses: [verificationCode.email] },
@@ -38,8 +54,6 @@ export const emailCode = async (verificationCode: {
   });
 
   await client.send(command);
-
-  // send the email
 };
 
 export const emailInvitation = async (membership: {
@@ -52,20 +66,35 @@ export const emailInvitation = async (membership: {
     throw new Error("This team does not exist.");
   }
 
+  // create SESv2 object to create template
+  const sesv2 = new SESv2({ region: process.env.REGION });
+
+  // try to create template; might already exist?
+  try {
+    const emailBody = fs.readFileSync("../res/invitation-email.html", "utf8");
+
+    const template = {
+      Subject: "You have been invited to join Inforest",
+      Html: emailBody,
+    };
+
+    sesv2.createEmailTemplate({
+      TemplateName: "inforest-invitation",
+      TemplateContent: template,
+    });
+  } catch (e: any) {
+    console.log("template exists?");
+    console.log(e);
+  }
+
+  // send email
   const client = new SESv2Client({ region: process.env.REGION });
 
   const command = new SendEmailCommand({
     Content: {
-      Template: { TemplateData: "" },
-      Simple: {
-        Body: {
-          Html: {
-            Data: "",
-          },
-        },
-        Subject: {
-          Data: "You have been invited to join Inforest",
-        },
+      Template: {
+        TemplateName: "inforest-invitation",
+        TemplateData: '{ "team_name":"' + team.name + '" }',
       },
     },
     Destination: { ToAddresses: [membership.email] },
