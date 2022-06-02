03ac6be9f48c0faf9a47a714a4a2c5d2019e171a
miruna <miruna.palaghean@gmail.com>
2022-06-02 00:59:11 -0400

    made login page appear before map screen

--- packages/app/App.tsx
+++ packages/app/App.tsx
@@ -6,6 +6,7 @@ import * as ScreenOrientation from "expo-screen-orientation";
 import useCachedResources from "./hooks/useCachedResources";
 import store from "./redux";
 import MapScreen from "./screens/MapScreen";
+import AuthScreen from "./screens/AuthScreen";
 import { useEffect } from "react";
 
 export default function App() {
@@ -17,10 +18,17 @@ export default function App() {
     ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.PORTRAIT_UP);
   }, []);
 
+  const isLoggedIn = store.user.token != "";
+
   return (
     <Provider store={store}>
       {/* <PersistGate loading={null} persistor={persistedStore}> */}
-      {isLoadingComplete && (
+      {isLoadingComplete && !isLoggedIn && (
+        <SafeAreaProvider>
+          <AuthScreen />
+        </SafeAreaProvider>
+      )}
+      {isLoadingComplete && isLoggedIn && (
         <SafeAreaProvider>
           <MapScreen />
           <StatusBar />
