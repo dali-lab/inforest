c3e84494b9428736e17544d48116ab420235ccda
Julian George <juliancgeorge@gmail.com>
2022-06-24 12:17:38 -0400

    changes to frontend url and adding seeder to procfile

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-release: cd packages/server && yarn sequelize db:migrate
+release: cd packages/server && yarn sequelize db:migrate && yarn sequelize db:seed:all
 web: cd packages/server && yarn start
--- packages/app/constants/Url.ts
+++ packages/app/constants/Url.ts
@@ -2,6 +2,6 @@ import { LOCAL_SERVER_URL } from "./dev";
 
 const SERVER_URL = __DEV__
   ? LOCAL_SERVER_URL
-  : "https://inforest.herokuapp.com/";
+  : "https://inforest-julian.herokuapp.com/";
 
 export default SERVER_URL;
