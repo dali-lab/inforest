e2b54a42883bee7048498fe0973bc5633b8e5718
Julian George <juliancgeorge@gmail.com>
2022-06-25 21:24:07 -0400

    updating frontend url, updating package.jsons

--- packages/app/constants/Url.ts
+++ packages/app/constants/Url.ts
@@ -2,6 +2,6 @@ import { LOCAL_SERVER_URL } from "./dev";
 
 const SERVER_URL = __DEV__
   ? LOCAL_SERVER_URL
-  : "https://inforest-julian.herokuapp.com/";
+  : "https://inforest-backend.onrender.com/";
 
 export default SERVER_URL;
