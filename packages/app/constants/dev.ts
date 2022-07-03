47ec6e23061f5d6e9925299ccc79c79e652640e0
11Some1 <68611574+11Some1@users.noreply.github.com>
2022-07-03 01:01:14 -0400

    resolve merge conflicts

--- packages/app/constants/dev.ts
+++ packages/app/constants/dev.ts
@@ -3,11 +3,7 @@
  * Configure this to your local development environment.
  */
 
-<<<<<<< HEAD
 export const LOCAL_SERVER_URL = "https://inforest-backend.onrender.com/";
-=======
-export const LOCAL_SERVER_URL = "http://172.20.10.7:3000/";
->>>>>>> 0e95638ca9d35bd65b13a94581318819b075e536
 export const FOREST_ID = "53dfd605-8189-44c7-ac9a-4b6ef8a203cf";
 export const TRIP_ID = "f03c4244-55d2-4f59-b5b1-0ea595982476";
 export const AUTHOR_ID = "24ea9f85-5352-4f69-b642-23291a27ff1e";
