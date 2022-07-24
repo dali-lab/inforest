7c5fb723d68f254f2ada8e068d5a10fb51ee3256
Eric Lu <68611574+eric-lu-VT@users.noreply.github.com>
2022-07-24 15:20:14 -0400

    Update dev.ts

--- packages/app/constants/dev.ts
+++ packages/app/constants/dev.ts
@@ -2,6 +2,5 @@
  * @developer
  * Configure this to your local development environment.
  */
-
-// https://localhost:3000/
-export const LOCAL_SERVER_URL = "https://inforest-backend.onrender.com/";
+ 
+export const LOCAL_SERVER_URL = "https://localhost:3000/";
