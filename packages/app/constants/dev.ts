6e9ddc955f1a5353b79b471922f391fd4ff4a8fa
Eric Lu <68611574+eric-lu-VT@users.noreply.github.com>
2022-07-25 19:40:59 -0400

    Update dev.ts

--- packages/app/constants/dev.ts
+++ packages/app/constants/dev.ts
@@ -3,5 +3,4 @@
  * Configure this to your local development environment.
  */
 
-// http://localhost:3000/
-export const LOCAL_SERVER_URL = "http://192.168.1.16:3000/";
+export const LOCAL_SERVER_URL = "http://localhost:3000/";
